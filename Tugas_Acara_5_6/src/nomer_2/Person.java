1f72b8ff4ac6c3a835ba738d94d2c8f4bb30aef5
dimasdwi15 <dimasdwinugroho15@gmail.com>
2025-02-10 09:52:19 +0700

    Create Person.java

---
+++ Tugas_Acara_5_6/src/nomer_2/Person.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nomer_2;
+
+/**
+ *
+ * @author Acer
+ */
+public class Person {
+     String name;
+     int age;
+     
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+       
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
