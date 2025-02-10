69059215d6e0a846d6d9d7b119361622ce75291f
dimasdwi15 <dimasdwinugroho15@gmail.com>
2025-02-10 09:47:57 +0700

    Create Nomer_1.java

---
+++ Tugas_Acara_5_6/src/nomer_1/Nomer_1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nomer_1;
+
+/**
+ *
+ * @author Acer
+ */
+public class Nomer_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      String nama = "Lisa Palombo";
+      int ID = 123456789;
+      String status = "Active";
+      
+        System.out.println("Nama = "+nama); 
+        System.out.println("ID = " + ID);
+        System.out.println("Status = " + status);
+      
+    }
+    
+}
