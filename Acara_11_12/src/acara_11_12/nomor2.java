b7d9ea31192634f56a4fdca6691d02059aa814a6
dimasdwi15 <dimasdwinugroho15@gmail.com>
2025-02-20 15:53:37 +0700

    Update nomor2.java

--- Acara_11_12/src/acara_11_12/nomor2.java
+++ Acara_11_12/src/acara_11_12/nomor2.java
@@ -10,25 +10,20 @@ import java.util.Scanner;
 public class nomor2 {
     public static void main(String[] args) {
 
-        // Membuat objek Scanner untuk menerima input dari pengguna
         Scanner scanner = new Scanner(System.in);
-
-        // Meminta input jumlah deret dari pengguna
+      
         System.out.print("Masukkan jumlah deret array: ");
         int jumlahDeret = scanner.nextInt();
 
-        // Membuat objek Random untuk menghasilkan angka acak
+       
         Random random = new Random();
 
-        // Membuat array untuk menyimpan angka acak
         int[] randomArray = new int[jumlahDeret];
-
-        // Mengisi array dengan angka acak antara 1 sampai 100
+   
         for (int i = 0; i < jumlahDeret; i++) {
-                randomArray[i] = random.nextInt(100) + 1;  // Angka acak antara 1 dan 100
+                randomArray[i] = random.nextInt(100) + 1;  
         }
 
-        // Menampilkan array yang dihasilkan
         System.out.print("Array acak yang di3hasilkan: [");
         for (int i = 0; i < randomArray.length; i++) {
             System.out.print(randomArray[i]);
